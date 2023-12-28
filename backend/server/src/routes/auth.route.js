334419cd4331e581be1ffcf19f74b17cf677dfab
AM-SagarThakare <sagar.thakare@angularminds.in>
2023-12-28 12:45:44 +0530

    backend starter kit added

---
+++ backend/server/src/routes/auth.route.js
@@ -0,0 +1,55 @@
+const logger = require("../config/logger");
+const { authController } = require("../controllers");
+const auth = require("../middlewares/auth");
+const captcha = require("../middlewares/captcha");
+const validate = require("../middlewares/validate");
+const { authValidation } = require("../validations");
+
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  res.send("hiii in auth /");
+});
+
+router.post(
+  "/register",
+  [captcha.verify, validate(authValidation.register)],
+  authController.register
+);
+
+router.post(
+  "/login",
+  [captcha.verify, validate(authValidation.login)],
+  authController.login
+);
+
+router.post(
+  "/login/:provider",
+  [captcha.verify, validate(authValidation.socialLogin)],
+  authController.socialLogin
+);
+
+router.post(
+  "/forgot-password",
+  [captcha.verify, validate(authValidation.forgotPassword)],
+  authController.forgotPassword
+);
+
+router.post(
+  "/reset-password",
+  validate(authValidation.resetPassword),
+  authController.resetPassword
+);
+
+router.post(
+  "/send-verification-email",
+  [auth()],
+  authController.sendVerificationEmail
+);
+
+router.post(
+  "/verify-email",
+  validate(authValidation.verifyEmail),
+  authController.verifyEmail
+);
+module.exports = router;
