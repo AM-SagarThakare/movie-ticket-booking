a4f4752b9d8cd9d47f96630ac940805b27b17b3d
AM-SagarThakare <sagar.thakare@angularminds.in>
2024-01-03 10:54:52 +0530

    select seat logic added

--- frontend/src/components/Seats/Seats.jsx
+++ frontend/src/components/Seats/Seats.jsx
@@ -1,33 +1,41 @@
-import React, { useState } from "react";
 
-function Seats({ selectedSeats, bookedSeats, temporaryBlockedSeats, setSelectedSeats }) {
-  //   console.log(temporaryBlockedSeats);
-  // const [selected, setSelected] = useState(false)
+function Seats({
+  selectedSeats,
+  bookedSeats,
+  temporaryBlockedSeats,
+  setSelectedSeats,
+}) {
 
+  // create chairs according to the capacity of theatre
   const chairs = Array.from(Array(70), (_, index) => index + 1);
+
   const isTemporaryBlocked = (ele) => temporaryBlockedSeats?.includes(ele);
   const isAlreadyBooked = (ele) => bookedSeats?.includes(ele);
-  const isSelected = (ele) => selectedSeats?.includes(ele)
+  const isSelected = (ele) => selectedSeats?.includes(ele);
+
+  // select/seselect seat if it is not booked already, not present in already booked/temporary booked seats
+  const bookMySeat = (ele) => {
+    if (!bookedSeats.includes(ele) && !temporaryBlockedSeats.includes(ele)) {
+      if (selectedSeats.includes(ele)) {
+        setSelectedSeats(selectedSeats.filter((items) => items !== ele));
+      } else setSelectedSeats((prev) => [...prev, ele]);
+    }
+  };
 
   return chairs.map((ele, ind) => (
     <span
       className={`empty-seat p-1 cursor rounded d-flex justify-content-center align-items-center 
-                ${isAlreadyBooked(ele)
-          ? "booked-seat"
-          : isTemporaryBlocked(ele)
-            ? "temporary-blocked"
-            : ""
-        }
+                ${
+                  isAlreadyBooked(ele)
+                    ? "booked-seat"
+                    : isTemporaryBlocked(ele)
+                    ? "temporary-blocked"
+                    : ""
+                }
                 ${isSelected(ele) ? "selected-seat" : ""}
-                
         `}
       key={ind}
-      onClick={() => {
-        if (selectedSeats.includes(ele)) {
-          setSelectedSeats(selectedSeats.filter(items => items !== ele))
-        } else
-          setSelectedSeats((prev) => [...prev, ele])
-      }}
+      onClick={() => bookMySeat(ele)}
     >
       {ele}
     </span>
